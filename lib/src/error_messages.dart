589e3e3e704e3f918ef2742881b46e2fbf0f3127
Huzaif Fahad <huzaif@Huzaifs-MacBook-Pro.local>
2023-06-12 15:55:42 +0530

    Added several validators
    these validators include : strong password, account number, upi ID, indian zip/postal code, credit card number

--- lib/src/error_messages.dart
+++ lib/src/error_messages.dart
@@ -1,4 +1,6 @@
+
 class FieldGuardErrorMessages {
+  
   String rangeMinErrorMessage(int min) => 'Minimum : $min';
 
   String rangeMaxErrorMessage(int max) => 'Maximum : $max';
@@ -17,4 +19,15 @@ class FieldGuardErrorMessages {
 
   String invalidLengthErrorMessage(int len) =>
       'length should be equal to : $len';
+  
+  String weakPasswordErrorMessage() =>
+      'Your password should contain atleast 8 characters, \n1 uppercase, \n1 lowercase, \n1 number and \n1 special character';
+
+  String invalidAccountNumberErrorMessage() => 'The account number is invalid';
+
+  String invalidUpiIdErrorMessage() => 'The UPI ID is invalid';
+
+  String invalidZipCodeErrorMessage() => 'The zip code is invalid';
+
+  String invalidCardNumber() => 'The card number is invalid';
 }
